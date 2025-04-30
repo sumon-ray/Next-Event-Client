6d2eb93d62c22b0e7fd27c86c5aa55260164bcdb
zxayed <zzayedghost@gmail.com>
2025-05-01 00:48:40 +0600

    added color palette & font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import img from '../assets/images/banner1.png'
 
 export default function Home() {
   return (
-   <div>
-    <h1 className="text-3xl text-center bg-red-500">Home</h1>
+   <div className="h-[200dvh]">
+    
+    < div   className=""  
+   ></div>
+    <h1 className="text-3xl text-center ">Home</h1>
    </div>
   );
 }
